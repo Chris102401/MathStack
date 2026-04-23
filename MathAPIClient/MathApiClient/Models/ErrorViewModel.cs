47ad7c50882c106c441ef37ccbd17b90bf19987a
ST10441754 <ST10441754@vcconnect.edu.za>
2026-04-23 11:04:59 +0200

    Removed unnecessary MathAPIClient duplicate

--- MathAPIClient/MathApiClient/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MathApiClient.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
